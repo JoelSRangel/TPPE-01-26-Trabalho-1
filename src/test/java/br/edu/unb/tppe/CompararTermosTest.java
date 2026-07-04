4b0268992c4b16f0ed2e58b5d40c9d1f3738c826
JoelSRangel <joelsoaresrngl@gmail.com>
2026-07-04 11:56:59 -0300

    Adicionando Testes (Red) para o método executar() que será copiado da origem para o arquivo compararTermos.java

--- src/test/java/br/edu/unb/tppe/CompararTermosTest.java
+++ src/test/java/br/edu/unb/tppe/CompararTermosTest.java
@@ -12,4 +12,30 @@ public class CompararTermosTest {
         CompararTermos comparador = new CompararTermos(longos, curtos);
         assertNotNull(comparador);
     }
+
+    @Test
+    void deveRetornarTrueQuandoTermosForemIdenticos() {
+        String[] longos = {"Maria", "de", "Souza"};
+        String[] curtos = {"Maria", "Souza"};
+        CompararTermos comparador = new CompararTermos(longos, curtos);
+        assertTrue(comparador.executar());
+    }
+
+    @Test
+    void deveRetornarTrueQuandoCurtosForemIniciaisDosLongos() {
+        String[] longos = {"José", "da", "Silva"};
+        String[] curtos = {"J.", "Silva"};
+        
+        CompararTermos comparador = new CompararTermos(longos, curtos);
+        assertTrue(comparador.executar());
+    }
+
+    @Test
+    void deveRetornarFalseQuandoSobrenomesForemDiferentes() {
+        String[] longos = {"Carlos", "da", "Silva"};
+        String[] curtos = {"Carlos", "da", "Souza"};
+
+        CompararTermos comparador = new CompararTermos(longos, curtos);
+        assertFalse(comparador.executar());
+    }
 }
